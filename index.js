05b6108b8ac341cb15fba15a08e3c8db8ff87cae
KrisCrossMagick <github@knowhere.be>
2022-03-29 09:12:24 +0200

    setup the webserver with express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there');
+const express = require('express');
+
+const app = express();
+
+//lets setup the default route handler
+app.get('/', (req, res) => {
+	res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
+});
