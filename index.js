df3db79c0cc18637fd41abb0d08827b1bc172298
KrisCrossMagick <github@knowhere.be>
2022-03-29 18:58:58 +0200

    changed the manual middleware func to the package body-parser

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+//this next line adds the bodyParser to every single route in the code
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //lets setup the default route handler
 app.get('/', (req, res) => {
@@ -16,33 +19,12 @@ app.get('/', (req, res) => {
     `);
 });
 
-const bodyParser = (req, res, next) => {
-	if (req.method === 'POST') {
-		req.on('data', (data) => {
-			//this is the data received
-			//email=dev%40kriscrossmagick.com&password=qqqqqqq&passwordConfirmation=fefezefzef
-			//we will now split it on the '&' sign to receive key value pairs in the array "parsed"
-			const parsed = data.toString('utf8').split('&');
-			const formData = {};
-			for (let pair of parsed) {
-				//the pair now contains a string in the format email=kris.allaert@pm.me
-				//so we can assign a key value pair like so
-				const [ key, value ] = pair.split('=');
-				//and then we assign them to the formData object
-				formData[key] = value;
-			}
-			req.body = formData;
-			next();
-		});
-	}
-	else {
-		next();
-	}
-};
-
 //the bodyParser is a middleware function
 //it is possible to have multiple middleware function in a row, seperated by a ','
-app.post('/', bodyParser, (req, res) => {
+//ORIGINAL: app.post('/', bodyParser.urlencoded({ extended: true }), (req, res) => {
+//we moved it to the top under app = express() - so it will be used in ALL routing func as middleware func without
+//having to specify it every single time
+app.post('/', (req, res) => {
 	console.log(req.body);
 	res.send('info received!');
 });
