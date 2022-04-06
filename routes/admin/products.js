a7954f79c61507f7c81d5ef2eb869eac149b4c7c
KrisCrossMagick <github@knowhere.be>
2022-04-06 12:14:53 +0200

    add image upload

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,14 @@
+//external libraries
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+//internal files
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
-
+//declarations
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {});
 
@@ -14,15 +18,22 @@ router.get('/admin/products/new', (req, res) => {
 
 router.post(
 	'/admin/products/new',
+	//here we had to change the order of the middleware
+	//since bodyparser is not used, this comes second
+	//[ requireTitle, requirePrice ],
+	upload.single('image'),
 	[ requireTitle, requirePrice ],
 	async (req, res) => {
 		const errors = validationResult(req);
-		// if (!errors.isEmpty())
-		// 	return res.send(productsNewTemplate({ req, errors }));
+		//console.log(errors);
+		if (!errors.isEmpty()) return res.send(productsNewTemplate({ errors }));
 
-		req.on('data', (data) => {
-			console.log(data.toString());
-		});
+		//a base64 string can represent an image
+		//console.log(req.file.buffer.toString('base64'));
+		const image = req.file.buffer.toString('base64');
+		const { title, price } = req.body;
+
+		await productsRepo.create({ title, price, image });
 
 		return res.send('submitted');
 	}
